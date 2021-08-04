25c2979d2e0203ab6a962a10f8eebb1ecf841d36
Vyvy-vi <vyom.j@protonmail.com>
2021-08-04 15:30:21 +0530

    feat: Query constraints for routes

--- src/main.py
+++ src/main.py
@@ -23,19 +23,14 @@ def index():
     return responses.HTMLResponse(content=body)
 
 @app.get('/jokes/{joke_id}')
-def jokes_by_id(joke_id: int):
-    if joke_id > len(jokes):
-        raise HTTPException(
-            status_code=400,
-            detail=[{
-                "loc": [
-                    "path",
-                    "joke_id"
-                ],
-                "msg": f"Error: joke with ID = {joke_id} not found",
-                "type": "Bad Request"
-            }]
+def jokes_by_id(
+        joke_id: int = Path(
+            ...,
+            title="ID of the Joke to get",
+            ge=0,
+            le=len(jokes)
         )
+    ):
 
     return responses.JSONResponse(
         content={
@@ -45,23 +40,9 @@ def jokes_by_id(joke_id: int):
     )
 
 @app.get('/jokes')
-def _jokes(num: Optional[int] = None):
-    if not num:
-        num = len(jokes)
-
-    if num > len(jokes):
-        raise HTTPException(
-            status_code=400,
-            detail=[{
-                "loc": [
-                    "query",
-                    "num"
-                ],
-                "msg": f"Error: the API currently stores less than {num} jokes",
-                "type": "Bad Request"
-            }]
-        )
-
+def _jokes(
+        num: int = Query(1, ge=1, le=len(jokes))
+    ):
     random_joke_ids = sorted(random.sample(range(0, len(jokes)), num))
     return responses.JSONResponse(
         content=[
@@ -70,33 +51,9 @@ def _jokes(num: Optional[int] = None):
     )
 
 @app.get('/coinflip')
-def _coinflip(num: int = 1):
-    if num < 1:
-        raise HTTPException(
-            status_code=400,
-            detail=[{
-                "loc": [
-                    "query",
-                    "num"
-                ],
-                "msg": f"Error: num should be greater than 0",
-                "type": "Bad Request"
-            }]
-        )
-
-    if num > 10000:
-        raise HTTPException(
-            status_code=400,
-            detail=[{
-                "loc": [
-                    "query",
-                    "num"
-                ],
-                "msg": f"Error: num should be smaller than 10000",
-                "type": "Bad Request"
-            }]
-        )
-
+def _coinflip(
+        num: int = Query(1, ge=1, le=10000)
+    ):
     return responses.JSONResponse(
         content={
             "task": f"coinflip x {num}",
@@ -105,48 +62,14 @@ def _coinflip(num: int = 1):
     )
 
 @app.get('/diceroll')
-def _diceroll(num: int = 1, sides: int = 6):
-    if num < 1:
-        raise HTTPException(
-            status_code=400,
-            detail=[{
-                "loc": [
-                    "query",
-                    "num"
-                ],
-                "msg": "Error: num should be greater than 0",
-                "type": "Bad Request"
-            }]
-        )
-    if num > 10000:
-        raise HTTPException(
-            status_code=400,
-            detail=[{
-                "loc": [
-                    "query",
-                    "num"
-                ],
-                "msg": "Error: num should be smaller than 10000",
-                "type": "Bad Request"
-            }]
-        )
-    if sides > 100 or sides < 3:
-        raise HTTPException(
-            status_code=400,
-            detail=[{
-                "loc": [
-                    "query",
-                    "num"
-                ],
-                "msg": "Error: sides must be beween 3 and 100",
-                "type": "Bad Request"
-            }]
-        )
-
+def _diceroll(
+    num: int = Query(1, ge=1, le=10000),
+    sides: int = Query(6, ge=3, le=10000)
+    ):
     return responses.JSONResponse(
         content={
             "task": f"Diceroll - d{sides} x {num}",
-            "result": random.choices(range(1, sides), k=num)
+            "result": random.choices(range(1, sides + 1), k=num)
         }
     )
 
