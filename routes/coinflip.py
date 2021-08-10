3fceadf630156dea839146cc0a961a0fcab0db9b
Vyvy-vi <vyom.j@protonmail.com>
2021-08-10 22:31:10 +0530

    chore(format): format files

--- routes/coinflip.py
+++ routes/coinflip.py
@@ -1,4 +1,5 @@
 import random
+
 from fastapi import APIRouter, Query
 
 router = APIRouter()
@@ -8,9 +9,5 @@ router = APIRouter()
 def flip_coin(num: int = Query(1, ge=1, le=10000)):
     return {
         "task": f"coinflip x {num}",
-        "result": random.choices(
-            ["Heads", "Tails"],
-            k = num
-        )
+        "result": random.choices(["Heads", "Tails"], k=num),
     }
-
--- routes/jokes.py
+++ routes/jokes.py
@@ -1,9 +1,11 @@
 import random
-from fastapi import APIRouter, Query, Path
+
+from fastapi import APIRouter, Path, Query
 
 router = APIRouter()
 
 jokes = ["Jokes are going through", "hehe this is a joke", "more jokes"]
+
 
 @router.get("/jokes/all")
 def all_jokes():
@@ -16,19 +18,12 @@ def all_jokes():
 
 @router.get("/jokes/{joke_id}")
 def jokes_by_id(joke_id: int = Path(..., ge=0, le=len(jokes))):
-    return {
-        "joke": jokes[joke_id],
-        "id": joke_id
-    }
+    return {"joke": jokes[joke_id], "id": joke_id}
 
 
 @router.get("/jokes")
 def get_jokes(num: int = Query(1, ge=1, le=len(jokes))):
     random_ids = sorted(random.sample(range(len(jokes)), num))
     return {
-        "jokes": [
-            {"id": joke_id, "joke": jokes[joke_id]} for joke_id in random_ids
-        ]
+        "jokes": [{"id": joke_id, "joke": jokes[joke_id]} for joke_id in random_ids]
     }
-
-
