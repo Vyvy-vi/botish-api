aa6baeef31f8b4154e496433af5b42f6a003a9d9
Vyvy-vi <vyom.j@protonmail.com>
2021-08-10 22:48:38 +0530

    chore(fix): move back routes into src

--- src/routes/jokes.py
+++ src/routes/jokes.py
@@ -17,13 +17,13 @@ def all_jokes():
 
 
 @router.get("/jokes/{joke_id}")
-def jokes_by_id(joke_id: int = Path(..., ge=0, lt=len(jokes))):
-    return {"id": joke_id, "joke": jokes[joke_id]}
+def jokes_by_id(joke_id: int = Path(..., ge=0, le=len(jokes))):
+    return {"joke": jokes[joke_id], "id": joke_id}
 
 
 @router.get("/jokes")
-def get_jokes(num: int = Query(1, ge=1, lt=len(jokes))):
-    random_ids = sorted(random.sample(range(1, len(jokes)), num))
+def get_jokes(num: int = Query(1, ge=1, le=len(jokes))):
+    random_ids = sorted(random.sample(range(len(jokes)), num))
     return {
         "jokes": [{"id": joke_id, "joke": jokes[joke_id]} for joke_id in random_ids]
     }
